7b16650b5f3f76180ad8199cb8ca21bd86245588
GodlewskiKonrad <31934@student.pwsztar.edu.p>
2019-10-24 17:02:53 +0200

    add script

--- navbar.js
+++ navbar.js
@@ -1,8 +1,6 @@
 
 let nav ='<ul class="nav">'+
-                       '<li class="nav-item">'+
-                           '<a class="nav-link active" href="index.html">Home</a>'+
-                       '</li>'+
+                       '<li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>'+
                        '<li class="nav-item">'+
                            '<a class="nav-link" href="#">Blog</a>'+
                        '</li>'+
--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-
+let spacja1="    ";
+let zmienna="";
 for (i=1;i<=10;i++)
-{   console.log("\n");
+{
     for (j=1;j<=10;j++)
     {
-        console.log(i*j);
+        if (i*j<10){
+        zmienna+=(i*j)+spacja1;
+        }else{
+        zmienna+=(i*j)+"   ";
+        }
+
     }
+    zmienna+="\n";
 }
+console.log(zmienna);
